8c42357935616a8c2683dce583b92b948ff4860a
MatthiasDerler <5matthiasderler5@gmail.com>
2022-02-21 19:52:26 +0100

    commit

--- src/BioSim/Dit.cs
+++ src/BioSim/Dit.cs
@@ -2,12 +2,13 @@
 
 struct Dit
 {
+    private List<(int, float)> _innerConnections;
+    private List<(int, float)> _outerConnections;
     public Tuple<uint, uint> Position { get; set; }
-    public char[] Gene { get; }
-
-    public Dit(char[] gene, uint x, uint y)
+    public Dit(List<(int, float)> innerConnections, uint x, uint y)
     {
+        _innerConnections = new List<(int, float)>();
+        _outerConnections = new List<(int, float)>();
         Position = new Tuple<uint, uint>(x, y);
-        Gene = gene;
     }
 }
---
+++ src/BioSim/SimulationData.cs
@@ -0,0 +1,11 @@
+struct SimulationData
+{
+    private string[] _inputFunctionNames;
+
+    public SimulationData()
+    {
+        _inputFunctionNames = new string[] {
+
+        };
+    }
+}
