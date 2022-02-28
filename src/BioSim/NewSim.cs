7f01708ad617c81c96c795bd237ee2002f823eef
mderler <derler190063@sr.htlweiz.at>
2022-02-28 08:20:54 +0100

    commit

--- src/BioSim/NewSim.cs
+++ src/BioSim/NewSim.cs
@@ -22,6 +22,27 @@ class NewSimWindow : Window
     {
         VBox outer = new VBox();
         Add(outer);
+
+        HBox simNameBox = new HBox();
+        outer.Add(simNameBox);
+        Label simNameLabel = new Label("Simulation Name: ");
+        simNameBox.Add(simNameLabel);
+        Entry simNameEntry = new Entry();
+        simNameBox.Add(simNameEntry);
+
+        HBox worldSizeBox = new HBox();
+        outer.Add(worldSizeBox);
+        Label worldSizeLabel = new Label("World Size");
+        worldSizeBox.Add(worldSizeLabel);
+        Label worldXLabel = new Label("x:");
+        worldSizeBox.Add(worldXLabel);
+        Entry worldXEntry = new Entry();
+        worldSizeBox.Add(worldXEntry);
+        Label worldYLabel = new Label("y:");
+        worldSizeBox.Add(worldYLabel);
+        Entry worldYEntry = new Entry();
+        worldSizeBox.Add(worldYEntry);
+
         HBox buttonBox = new HBox();
         outer.Add(buttonBox);
 
@@ -32,11 +53,6 @@ class NewSimWindow : Window
         Button cancelButten = new Button("Cancel");
         cancelButten.Clicked += (object? sender, EventArgs e) => Destroy();
         buttonBox.Add(cancelButten);
-
-        Label simNameLabel = new Label("Simulation Name");
-        Label worldSize = new Label("World Size");
-        Label worldX = new Label("x");
-        Label worldy = new Label("y");
 
         ShowAll();
     }
