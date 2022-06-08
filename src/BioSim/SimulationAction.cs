62b1184cb7bef8d45fb5fea005bf0a6982d06f81
mderler <derler190063@sr.htlweiz.at>
2022-06-08 16:47:43 +0200

    commit

--- src/BioSim/SimulationAction.cs
+++ src/BioSim/SimulationAction.cs
@@ -8,11 +8,50 @@ public abstract class SimulationAction
 
     public string Error { get; protected set; }
 
-    // private (int gf, int gl, int sf, int sl)[]
+    private (uint gf, uint gl, uint sf, uint sl)[] _executeTimes;
 
     public SimulationAction(string eventString)
     {
         _actionString = eventString;
+
+        string[] allTimes = eventString.Split(';');
+        _executeTimes = new (uint, uint, uint, uint)[allTimes.Length];
+
+        for (int i = 0; i < allTimes.Length; i++)
+        {
+            uint[] times = new uint[4];
+            string[] tmp = allTimes[i].Split(',');
+
+            bool mistake = false;
+
+            for (int j = 0; j < 4; j++)
+            {
+                if (tmp[j] == "s")
+                {
+                    times[j] = 0;
+                    continue;
+                }
+                if (tmp[j] == "b")
+                {
+                    times[j] = uint.MaxValue;
+                    continue;
+                }
+                mistake = !uint.TryParse(tmp[j], out times[j]);
+                if (mistake)
+                {
+                    break;
+                }
+            }
+
+            if (mistake)
+            {
+                Error += "error reading times";
+                return;
+            }
+
+            _executeTimes[i] = (times[0], times[1], times[2], times[3]);
+        }
+
     }
 
     public void ExecuteAction()
